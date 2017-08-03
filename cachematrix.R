ebae14fcc60d40d2fb195cfae97a3a9c25772bb8
Bruce <git@gayleard.net>
2017-08-03 19:24:30 +1000

    my solution v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix: Takes a matrix as input and creates a special "cache matrix" object that
+#                  caches its inverse and tracks changes to the matrix last inversed.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inMatrix = matrix()) {
+    # initialise inverse & changed values
+    inverseMatrix <- NULL
+    changedMatrix <- TRUE
+    # set will set the matrix and initialise inverse & changed values
+    set <- function(setMatrix) {
+        inMatrix <<- setMatrix
+        inverseMatrix <<- NULL
+        changedMatrix <<- TRUE
+    }
+    # get returns the matrix
+    get <- function() inMatrix
+    # setinverse computes the inverse, caches it & sets the changed value
+    setinverse <- function(solve) {
+        inverseMatrix <<- solve(inMatrix)
+        changedMatrix <<- FALSE
+    }
+    # getinverse returns the cached inverse
+    getinverse <- function() inverseMatrix
+    # getchanged returns the value of changed
+    getchanged <- function() changedMatrix
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse,
+         getchanged = getchanged)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve: Takes as input a special "cache matrix" object created by makeCacheMatrix.
+#             If the matrix has not changed and the inverse has been cached, then cacheSolve
+#             retrieves the cached inverse, otherwise it computes the inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMatrix, ...) {
+    # check matrix data has not changed
+    cm <- cacheMatrix$getchanged()
+    if (!cm) {
+        message("matrix data not changed")
+        # matrix data unchanged, so get the cached inverse
+        cacheInverse <- cacheMatrix$getinverse()
+        if(!is.null(cacheInverse)) {
+            # cached inverse is not null, so return it
+            message("getting cached inverse")
+            return(cacheInverse)
+        }
+    }
+    # get the matrix data and compute the inverse
+    data <- cacheMatrix$get()
+    cacheInverse <- solve(data)
+    # cache the inverse
+    cacheMatrix$setinverse(cacheInverse)
+    # return the inverse
+    cacheInverse
 }
